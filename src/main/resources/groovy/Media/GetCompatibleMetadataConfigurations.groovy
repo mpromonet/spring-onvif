8ab6abe2c197669846b51304edaf106a467128e7
mpromonet <michel.promonet@free.fr>
2012-11-23 22:02:37 +0100

    implement metadataconfiguration

--- src/main/resources/groovy/Media/GetCompatibleMetadataConfigurations.groovy
+++ src/main/resources/groovy/Media/GetCompatibleMetadataConfigurations.groovy
@@ -1,2 +1,8 @@
 response = new org.onvif.ver10.media.wsdl.GetCompatibleMetadataConfigurationsResponse();
+ptzfilter = org.onvif.ver10.schema.PTZFilter();
+metadata = org.onvif.ver10.schema.MetadataConfiguration();
+metadata.setPTZStatus(ptzfilter);
+event = org.onvif.ver10.schema.EventSubscription();
+metadata.setPTZStatus(ptzfilter);
+response.getConfigurations().add(metadata);
 response;
