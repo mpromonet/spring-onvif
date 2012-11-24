9657e56f353f8dd45799c4d887c182c9954521c2
mpromonet <mic672@msn.com>
2012-11-24 14:10:22 +0100

    add discovery probe

--- src/main/resources/groovy/DiscoveryLookup/Probe.groovy
+++ src/main/resources/groovy/DiscoveryLookup/Probe.groovy
@@ -1,2 +1,7 @@
-response = new org.onvif.ver10.network.wsdl.ProbeResponse();
+client = new org.apache.cxf.ws.discovery.WSDiscoveryClient();
+client.setDefaultProbeTimeout(1000);
+
+probeType = new org.apache.cxf.ws.discovery.wsdl.ProbeType();
+response = client.probe(probeType);
+					
 response;
