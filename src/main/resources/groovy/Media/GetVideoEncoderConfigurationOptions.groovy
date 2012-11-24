8a43f4bed8f23ed141a45036c527545474efe1c3
mpromonet <mic672@msn.com>
2012-11-24 15:54:23 +0100

    fix video encoder configuration

--- src/main/resources/groovy/Media/GetVideoEncoderConfigurationOptions.groovy
+++ src/main/resources/groovy/Media/GetVideoEncoderConfigurationOptions.groovy
@@ -1,6 +1,12 @@
+frameRateRange = new org.onvif.ver10.schema.IntRange();
+frameRateRange.setMin(1);
+frameRateRange.setMax(25);
+
+h264Opts = new org.onvif.ver10.schema.H264Options();
+h264Opts.setFrameRateRange(frameRateRange);
+
 options = new org.onvif.ver10.schema.VideoEncoderConfigurationOptions();
-options.setConfigurationToken("configurationToken0");
-options.setProfileToken("profileToken0");
+options.setH264(h264Opts);
 
 response = new org.onvif.ver10.media.wsdl.GetVideoEncoderConfigurationOptionsResponse();
 response.setOptions(options);
