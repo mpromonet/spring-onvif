53470adf347643aa57e85d931771b76f393f0144
mpromonet <mic672@msn.com>
2012-12-02 23:39:40 +0100

    add more scopes

--- src/main/resources/groovy/Device/GetScopes.groovy
+++ src/main/resources/groovy/Device/GetScopes.groovy
@@ -1,13 +1,23 @@
-response = new org.onvif.ver10.device.wsdl.GetScopesResponse();
+def response = new org.onvif.ver10.device.wsdl.GetScopesResponse();
 
-scope = new org.onvif.ver10.schema.Scope();
+def scope = new org.onvif.ver10.schema.Scope();
 scope.setScopeDef(org.onvif.ver10.schema.ScopeDefinition.FIXED)
 scope.setScopeItem("onvif://www.onvif.org/name/" + properties.resolve("deviceid.name"))
 response.getScopes().add(scope);
 
-scope = new org.onvif.ver10.schema.Scope();
+def scope = new org.onvif.ver10.schema.Scope();
 scope.setScopeDef(org.onvif.ver10.schema.ScopeDefinition.FIXED)
 scope.setScopeItem("onvif://www.onvif.org/location/" + properties.resolve("deviceid.location"))
+response.getScopes().add(scope);
+
+def scope = new org.onvif.ver10.schema.Scope();
+scope.setScopeDef(org.onvif.ver10.schema.ScopeDefinition.FIXED)
+scope.setScopeItem("onvif://www.onvif.org/hardware/" + properties.resolve("deviceid.hardware"))
+response.getScopes().add(scope);
+
+def scope = new org.onvif.ver10.schema.Scope();
+scope.setScopeDef(org.onvif.ver10.schema.ScopeDefinition.FIXED)
+scope.setScopeItem("onvif://www.onvif.org/Profile/" + properties.resolve("deviceid.profile"))
 response.getScopes().add(scope);
 
 response;
