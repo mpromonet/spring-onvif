a13584a81b30db53cd2ddc1f2167c10c87ecf65e
mpromonet <mic672@msn.com>
2012-12-02 22:49:07 +0100

    decode input message of GetCapabilities

--- src/main/resources/groovy/Device/GetCapabilities.groovy
+++ src/main/resources/groovy/Device/GetCapabilities.groovy
@@ -1,31 +1,45 @@
-response = new org.onvif.ver10.device.wsdl.GetCapabilitiesResponse();
-capabilities =  new org.onvif.ver10.schema.Capabilities();
-extendCapability = new org.onvif.ver10.schema.CapabilitiesExtension();
+// unmarshall request
+// =================
+def soapMessage = request.getBody().getBody().get(0);
+def jaxbCtx = javax.xml.bind.JAXBContext.newInstance("org.onvif.ver10.device.wsdl");
+def unmarshaller = jaxbCtx.createUnmarshaller();
+def req = (org.onvif.ver10.device.wsdl.GetCapabilities) unmarshaller.unmarshal(soapMessage);
 
-url = request.getHeader("CamelCxfMessage")["http.base.path"]+ "/webservices";
+req.getCategory().each( { println "==> Category:" + it } );
 
-deviceCapability = new org.onvif.ver10.schema.DeviceCapabilities();
+// prepare answer
+// =================
+def url = request.getHeader("CamelCxfMessage")["http.base.path"]+ "/webservices";
+
+def rep = new org.onvif.ver10.device.wsdl.GetCapabilitiesResponse();
+def capabilities =  new org.onvif.ver10.schema.Capabilities();
+def extendCapability = new org.onvif.ver10.schema.CapabilitiesExtension();
+
+
+def deviceCapability = new org.onvif.ver10.schema.DeviceCapabilities();
 deviceCapability.setXAddr(url+"/Device");
 onvifversion = new org.onvif.ver10.schema.OnvifVersion();
 onvifversion.setMajor(properties.resolve("onvif.major").toInteger() );
 onvifversion.setMinor(properties.resolve("onvif.minor").toInteger());
-systemCapability = new org.onvif.ver10.schema.SystemCapabilities();
+
+def systemCapability = new org.onvif.ver10.schema.SystemCapabilities();
 systemCapability.getSupportedVersions().add(onvifversion);
 deviceCapability.setSystem(systemCapability);
-networkCapability = new org.onvif.ver10.schema.NetworkCapabilities();
+
+def networkCapability = new org.onvif.ver10.schema.NetworkCapabilities();
 deviceCapability.setNetwork(networkCapability);
 capabilities.setDevice(deviceCapability);
 
 if (properties.resolve("service.deviceio.enabled") != "0")
 {
-	deviceIOCapability = new org.onvif.ver10.schema.DeviceIOCapabilities();
+	def deviceIOCapability = new org.onvif.ver10.schema.DeviceIOCapabilities();
 	deviceIOCapability.setXAddr(url+"/DeviceIO");
 	extendCapability.setDeviceIO(deviceIOCapability);
 }
 
 if (properties.resolve("service.event.enabled") != "0")
 {
-	eventCapability = new org.onvif.ver10.schema.EventCapabilities();
+	def eventCapability = new org.onvif.ver10.schema.EventCapabilities();
 	eventCapability.setXAddr(url+"/EventPortType");
 	eventCapability.setWSPullPointSupport(true);
 	eventCapability.setWSSubscriptionPolicySupport(true);
@@ -34,12 +48,12 @@ if (properties.resolve("service.event.enabled") != "0")
 
 if (properties.resolve("service.media.enabled") != "0")
 {
-	streamingCapability = new org.onvif.ver10.schema.RealTimeStreamingCapabilities();
+	def streamingCapability = new org.onvif.ver10.schema.RealTimeStreamingCapabilities();
 	streamingCapability.setRTPMulticast(false);
 	streamingCapability.setRTPTCP(true);
 	streamingCapability.setRTPRTSPTCP(true);
 	
-	mediaCapability = new org.onvif.ver10.schema.MediaCapabilities();
+	def mediaCapability = new org.onvif.ver10.schema.MediaCapabilities();
 	mediaCapability.setXAddr(url+"/Media");
 	mediaCapability.setStreamingCapabilities(streamingCapability);
 	
@@ -48,26 +62,26 @@ if (properties.resolve("service.media.enabled") != "0")
 
 if (properties.resolve("service.ptz.enabled") != "0")
 {
-	ptzCapability = new org.onvif.ver10.schema.PTZCapabilities();
+	def ptzCapability = new org.onvif.ver10.schema.PTZCapabilities();
 	ptzCapability.setXAddr(url+"/PTZ");
 	capabilities.setPTZ(ptzCapability);
 }
 
 if (properties.resolve("service.replay.enabled") != "0")
 {
-	replayCapability = new org.onvif.ver10.schema.ReplayCapabilities();
+	def replayCapability = new org.onvif.ver10.schema.ReplayCapabilities();
 	replayCapability.setXAddr(url+"/Replay");
 	extendCapability.setReplay(replayCapability);
 }
 
 if (properties.resolve("service.recording.enabled") != "0")
 {
-	recordCapability = new org.onvif.ver10.schema.RecordingCapabilities();
+	def recordCapability = new org.onvif.ver10.schema.RecordingCapabilities();
 	recordCapability.setXAddr(url+"/Recording");
 	extendCapability.setRecording(recordCapability);	
 }
 					
 capabilities.setExtension(extendCapability);					
-response.setCapabilities(capabilities);
+rep.setCapabilities(capabilities);
 
-response;
+rep;
