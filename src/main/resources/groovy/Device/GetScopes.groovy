d7f6318fdf1f6fe681a95b7a649460bf224ce527
mpr <mic672@msn.com>
2012-12-26 23:05:55 +0100

    fix GetScopes processing

--- src/main/resources/groovy/Device/GetScopes.groovy
+++ src/main/resources/groovy/Device/GetScopes.groovy
@@ -5,24 +5,24 @@ scope.setScopeDef(org.onvif.ver10.schema.ScopeDefinition.FIXED)
 scope.setScopeItem("onvif://www.onvif.org/name/" + properties.resolve("deviceid.name"))
 response.getScopes().add(scope);
 
-def scope = new org.onvif.ver10.schema.Scope();
+scope = new org.onvif.ver10.schema.Scope();
 scope.setScopeDef(org.onvif.ver10.schema.ScopeDefinition.FIXED)
 scope.setScopeItem("onvif://www.onvif.org/location/" + properties.resolve("deviceid.location"))
 response.getScopes().add(scope);
 
-def scope = new org.onvif.ver10.schema.Scope();
+scope = new org.onvif.ver10.schema.Scope();
 scope.setScopeDef(org.onvif.ver10.schema.ScopeDefinition.FIXED)
 scope.setScopeItem("onvif://www.onvif.org/hardware/" + properties.resolve("deviceid.hardware"))
 response.getScopes().add(scope);
 
-def scope = new org.onvif.ver10.schema.Scope();
+scope = new org.onvif.ver10.schema.Scope();
 scope.setScopeDef(org.onvif.ver10.schema.ScopeDefinition.FIXED)
 scope.setScopeItem("onvif://www.onvif.org/Profile/" + properties.resolve("deviceid.profile"))
 response.getScopes().add(scope);
 
 if (properties.resolve("service.ptz.enabled") != "0")
 {
-	def scope = new org.onvif.ver10.schema.Scope();
+	scope = new org.onvif.ver10.schema.Scope();
 	scope.setScopeDef(org.onvif.ver10.schema.ScopeDefinition.FIXED)
 	scope.setScopeItem("onvif://www.onvif.org/type/ptz")
 	response.getScopes().add(scope);
@@ -30,7 +30,7 @@ if (properties.resolve("service.ptz.enabled") != "0")
 
 if (properties.resolve("service.recording.enabled") != "0")
 {
-	def scope = new org.onvif.ver10.schema.Scope();
+	scope = new org.onvif.ver10.schema.Scope();
 	scope.setScopeDef(org.onvif.ver10.schema.ScopeDefinition.FIXED)
 	scope.setScopeItem("onvif://www.onvif.org/type/Network_Video_Storage")
 	response.getScopes().add(scope);
@@ -38,7 +38,7 @@ if (properties.resolve("service.recording.enabled") != "0")
 
 if (properties.resolve("service.media.enabled") != "0")
 {
-	def scope = new org.onvif.ver10.schema.Scope();
+	scope = new org.onvif.ver10.schema.Scope();
 	scope.setScopeDef(org.onvif.ver10.schema.ScopeDefinition.FIXED)
 	scope.setScopeItem("onvif://www.onvif.org/type/Network_Video_Transmitter")
 	response.getScopes().add(scope);
