c769efc93d1756ab3a78cf1bd9b63dd2621f4aad
mpromonet <mic672@msn.com>
2013-01-27 23:20:45 +0100

    createpullpoint : fill mandatory currenttime

--- src/main/resources/groovy/EventPortType/CreatePullPointSubscription.groovy
+++ src/main/resources/groovy/EventPortType/CreatePullPointSubscription.groovy
@@ -13,6 +13,17 @@ refbuilder.address(url+pullpoint.getAddress());
 refbuilder.serviceName(pullpoint.getServiceName());
 refbuilder.wsdlDocumentLocation(pullpoint.getWsdlURL());
 
+def currentdate = new java.util.GregorianCalendar();
+def xmlDate = javax.xml.datatype.DatatypeFactory.newInstance().newXMLGregorianCalendar(currentdate.get(java.util.Calendar.YEAR),
+																		currentdate.get(java.util.Calendar.MONTH) + 1,
+																		currentdate.get(java.util.Calendar.DAY_OF_MONTH),
+																		currentdate.get(java.util.Calendar.HOUR_OF_DAY),
+																		currentdate.get(java.util.Calendar.MINUTE),
+																		currentdate.get(java.util.Calendar.SECOND),
+																		currentdate.get(java.util.Calendar.MILLISECOND), 0);
+
 response = new org.onvif.ver10.events.wsdl.CreatePullPointSubscriptionResponse();
 response.setSubscriptionReference(refbuilder.build());
+response.setCurrentTime(xmlDate);
+
 response;
