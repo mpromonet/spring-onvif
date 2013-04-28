fc1c74f35f7d8b85887f17e7446067b062431acd
mpromonet <mic672@msn.com>
2013-04-28 22:48:25 +0200

    media profile stuff

--- src/main/resources/groovy/Media/GetProfile.groovy
+++ src/main/resources/groovy/Media/GetProfile.groovy
@@ -1,5 +1,17 @@
-def profile =  context.getApplicationContext().getBean('profile');
+// unmarshall request
+// =================
+def soapMessage = request.getBody().getBody().get(0);
+def jaxbCtx = javax.xml.bind.JAXBContext.newInstance("org.onvif.ver10.media.wsdl");
+def unmarshaller = jaxbCtx.createUnmarshaller();
+def req = (org.onvif.ver10.media.wsdl.GetProfile) unmarshaller.unmarshal(soapMessage);
+println "==> " + req.getProfileToken() 
 
 response = new org.onvif.ver10.media.wsdl.GetProfileResponse();
-response.setProfile(profile);
+
+if (req.getProfileToken() == "profile")
+{
+	def profile =  context.getApplicationContext().getBean('profile');
+	response.setProfile(profile);
+}
+
 response;
