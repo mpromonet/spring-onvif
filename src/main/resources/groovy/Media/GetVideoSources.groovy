c2f97ea40a0ce455420c0e4ad3e3a115b5ab3401
mpromonet <mic672@msn.com>
2013-05-19 00:52:30 +0200

    media/getvideosources

--- src/main/resources/groovy/Media/GetVideoSources.groovy
+++ src/main/resources/groovy/Media/GetVideoSources.groovy
@@ -1,11 +1,6 @@
 response = new org.onvif.ver10.media.wsdl.GetVideoSourcesResponse();
 
-source = new org.onvif.ver10.schema.VideoSource()
-source.setToken("videotoken1");
-response.getVideoSources().add(source);
-
-source = new org.onvif.ver10.schema.VideoSource()
-source.setToken("videotoken2");
-response.getVideoSources().add(source);
+response.getVideoSources().add(context.getApplicationContext().getBean('videoSource1'));
+response.getVideoSources().add(context.getApplicationContext().getBean('videoSource2'));
 
 response;
