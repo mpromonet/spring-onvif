0b6f87c53359ea7b9dc9f26fc0d7d59d18cd76ed
mpromonet <mic672@msn.com>
2013-05-18 21:39:45 +0200

    device/getNetworkInterfaces : fill mac address

--- src/main/resources/groovy/Device/GetNetworkInterfaces.groovy
+++ src/main/resources/groovy/Device/GetNetworkInterfaces.groovy
@@ -1,7 +1,9 @@
 response = new org.onvif.ver10.device.wsdl.GetNetworkInterfacesResponse();
 
+def ip = java.net.InetAddress.getLocalHost(); 
+
 linkLocal = new org.onvif.ver10.schema.PrefixedIPv4Address();
-linkLocal.setAddress(java.net.InetAddress.getLocalHost().getHostAddress());
+linkLocal.setAddress(ip.getHostAddress());
 linkLocal.setPrefixLength(24);
 
 ipv4Config = new org.onvif.ver10.schema.IPv4Configuration();
@@ -12,9 +14,14 @@ ipv4 = new org.onvif.ver10.schema.IPv4NetworkInterface();
 ipv4.setEnabled(true);
 ipv4.setConfig(ipv4Config);
 
+def mac = java.net.NetworkInterface.getByInetAddress(ip).getHardwareAddress();
+interfaceInfo = new org.onvif.ver10.schema.NetworkInterfaceInfo();
+interfaceInfo.setHwAddress(mac.encodeHex().toString());
+
 network = new org.onvif.ver10.schema.NetworkInterface();
 network.setEnabled(true);
 network.setIPv4(ipv4);				
+network.setInfo(interfaceInfo);				
 response.getNetworkInterfaces().add(network);
 
 response;
