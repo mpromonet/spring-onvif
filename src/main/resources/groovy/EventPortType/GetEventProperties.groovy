7c3e3bdcb57d4eba1b2cf29a0ba4cb175d9156e7
mpromonet <mic672@msn.com>
2013-05-19 01:17:29 +0200

    event/geteventproperties : print soap header

--- src/main/resources/groovy/EventPortType/GetEventProperties.groovy
+++ src/main/resources/groovy/EventPortType/GetEventProperties.groovy
@@ -1,3 +1,7 @@
+// SOAP headers
+// =================
+request.getBody().getHeaders().each( { println "==> SOAP Header: {" + it.getObject().getNamespaceURI()+ "}:"+ it.getObject().getLocalName()  + "=" + it.getObject().getTextContent() } );
+
 topic = new org.oasis_open.docs.wsn.t_1.TopicSetType();
 
 response = new org.onvif.ver10.events.wsdl.GetEventPropertiesResponse();
