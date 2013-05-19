8130de87d954455ae62f4432423667554157c70c
mpromonet <mic672@msn.com>
2013-05-19 02:10:11 +0200

    media

--- src/main/resources/groovy/Media/GetCompatibleMetadataConfigurations.groovy
+++ src/main/resources/groovy/Media/GetCompatibleMetadataConfigurations.groovy
@@ -1,11 +1,3 @@
-def ptzfilter = new org.onvif.ver10.schema.PTZFilter();
-def event = new org.onvif.ver10.schema.EventSubscription();
-
-def metadata = new org.onvif.ver10.schema.MetadataConfiguration();
-metadata.setPTZStatus(ptzfilter);
-metadata.setEvents(event);
-metadata.setAnalytics(false);
-
 def response = new org.onvif.ver10.media.wsdl.GetCompatibleMetadataConfigurationsResponse();
-response.getConfigurations().add(metadata);
+response.getConfigurations().add(context.getApplicationContext().getBean('metadataCfg'));
 response;
--- src/main/resources/groovy/Media/GetVideoEncoderConfigurationOptions.groovy
+++ src/main/resources/groovy/Media/GetVideoEncoderConfigurationOptions.groovy
@@ -1,12 +1,25 @@
+// unmarshall request
+// =================
+def soapMessage = request.getBody().getBody().get(0);
+def jaxbCtx = javax.xml.bind.JAXBContext.newInstance("org.onvif.ver10.media.wsdl");
+def unmarshaller = jaxbCtx.createUnmarshaller();
+def req = (org.onvif.ver10.media.wsdl.GetVideoEncoderConfigurationOptions) unmarshaller.unmarshal(soapMessage);
+println "==> configuration:" + req.getConfigurationToken()  + " profile:"+ req.getProfileToken()
+
 frameRateRange = new org.onvif.ver10.schema.IntRange();
 frameRateRange.setMin(1);
 frameRateRange.setMax(25);
+
+qualityRange = new org.onvif.ver10.schema.IntRange();
+qualityRange.setMin(1);
+qualityRange.setMax(25);
 
 h264Opts = new org.onvif.ver10.schema.H264Options();
 h264Opts.setFrameRateRange(frameRateRange);
 
 options = new org.onvif.ver10.schema.VideoEncoderConfigurationOptions();
 options.setH264(h264Opts);
+options.setQualityRange(qualityRange);
 
 response = new org.onvif.ver10.media.wsdl.GetVideoEncoderConfigurationOptionsResponse();
 response.setOptions(options);
