ebd6d911a4d9de6e29b316da211a4488231909a9
mpr <mic672@msn.com>
2013-06-01 13:36:38 +0200

    event : pullmessages

--- src/main/resources/groovy/PullPointSubscription/PullMessages.groovy
+++ src/main/resources/groovy/PullPointSubscription/PullMessages.groovy
@@ -2,10 +2,22 @@ def rep = new org.onvif.ver10.events.wsdl.PullMessagesResponse();
 
 for ( i in 0..9 ) 
 {
-	def elt = new javax.xml.bind.JAXBElement<String>(new javax.xml.namespace.QName("ns","name"),String.class, "message" + i);
+	def item = new org.onvif.ver10.schema.ItemList.SimpleItem();
+	item.setName("content");
+	item.setValue( "message" + i);
+	
+	def itemList = new org.onvif.ver10.schema.ItemList();
+	itemList.getSimpleItem().add(item);
+	
+	def msg = new org.onvif.ver10.schema.Message();
+	msg.setData(itemList);
+	
+	def domResult = new javax.xml.transform.dom.DOMResult();
+	def marshaller = javax.xml.bind.JAXBContext.newInstance("org.onvif.ver10.schema").createMarshaller();
+	marshaller.marshal(msg,domResult);
 
 	def message = new org.oasis_open.docs.wsn.b_2.NotificationMessageHolderType.Message();
-	message.setAny(elt);
+	message.setAny(domResult.getNode().getDocumentElement());
 
 	def notification = new org.oasis_open.docs.wsn.b_2.NotificationMessageHolderType();
 	notification.setMessage(message);
